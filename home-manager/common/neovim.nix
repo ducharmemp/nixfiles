2928495974b5e722becb2c37c7c60c3e0b5fe2e6
Matthew DuCharme <ducharmemp@gmail.com>
2024-03-08 21:12:07 -0500

    Neovim config addition

--- home-manager/common/neovim.nix
+++ home-manager/common/neovim.nix
@@ -6,9 +6,16 @@
     vimAlias = true;
     vimdiffAlias = true;
 
+    extraLuaConfig = builtins.readFile ./kickstart.lua;
+
     plugins = with pkgs; [
         vimPlugins.nvim-cmp
         vimPlugins.telescope-nvim
+        vimPlugins.nvim-treesitter.withAllGrammars
+        vimPlugins.nvim-colorizer-lua
+        vimPlugins.which-key-nvim
+        vimPlugins.nvim-lspconfig
+        vimPlugins.nordic-nvim
     ];
   };
 }
