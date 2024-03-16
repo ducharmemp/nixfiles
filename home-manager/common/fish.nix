7c679d9170e4fe7b6eb232fa8046d8fa03f61f54
Matthew DuCharme <ducharmemp@gmail.com>
2024-03-15 21:03:31 -0400

    Neovim tweaks

--- home-manager/common/fish.nix
+++ home-manager/common/fish.nix
@@ -1,13 +1,14 @@
 {pkgs, ...}: {
     programs.fish = {
-    enable = true;
-    shellInit = ''
-      set fish_greeting
-      for p in (string split " " $NIX_PROFILES); fish_add_path --prepend --move $p/bin; end
-    '';
-    plugins = [
-        { name = "fzf-fish"; inherit (pkgs.fishPlugins.fzf-fish) src; }
-        { name = "tide"; inherit (pkgs.fishPlugins.tide) src; }
-    ];
+        enable = true;
+        shellInit = ''
+          set fish_greeting
+          for p in (string split " " $NIX_PROFILES); fish_add_path --prepend --move $p/bin; end
+          test -O ~/.aliases && source ~/.aliases
+        '';
+        plugins = [
+            { name = "fzf-fish"; inherit (pkgs.fishPlugins.fzf-fish) src; }
+            { name = "tide"; inherit (pkgs.fishPlugins.tide) src; }
+        ];
   };
 }
