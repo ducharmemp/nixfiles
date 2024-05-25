aa127c067e5334e33e35b592df9df60d0505ac64
Matthew DuCharme <ducharmemp@gmail.com>
2024-05-25 16:55:40 +0000

    Adding git info to statusline

--- home-manager/common/fish.nix
+++ home-manager/common/fish.nix
@@ -5,7 +5,6 @@
           set fish_greeting
           for p in (string split " " $NIX_PROFILES); fish_add_path --prepend --move $p/bin; end
           test -O ~/.aliases && source ~/.aliases
-          for p in /Users/matthewducharme/.local/share/gem/ruby/*; fish_add_path --prepend $p/bin; end
         '';
         plugins = [
             { name = "fzf-fish"; inherit (pkgs.fishPlugins.fzf-fish) src; }
