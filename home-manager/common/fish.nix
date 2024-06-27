4fc2ede009a44469cbfcc4f844a349da12d04acd
Matthew DuCharme <ducharmemp@gmail.com>
2024-06-27 12:37:43 -0400

    Adding vi mode to fish shell

--- home-manager/common/fish.nix
+++ home-manager/common/fish.nix
@@ -8,6 +8,7 @@
           for p in $HOME/.local/share/gem/ruby/*; fish_add_path --prepend $p/bin; end
           for p in /Applications/WezTerm.app/Contents/MacOS*; fish_add_path --prepend $p; end
           wezterm shell-completion --shell fish > $HOME/.config/fish/completions/wezterm.fish
+          set -g fish_key_bindings fish_vi_key_bindings
         '';
         plugins = [
             { name = "fzf-fish"; inherit (pkgs.fishPlugins.fzf-fish) src; }
