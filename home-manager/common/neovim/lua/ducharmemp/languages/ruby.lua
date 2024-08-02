dd7c7bda477034e35eb8c795d8212f845de40981
Matthew DuCharme <ducharmemp@gmail.com>
2024-08-02 10:24:30 -0400

    Updating test command to be able to rerun the command

--- home-manager/common/neovim/lua/ducharmemp/languages/ruby.lua
+++ home-manager/common/neovim/lua/ducharmemp/languages/ruby.lua
@@ -1,27 +1,11 @@
-local function err(e)
-	vim.notify("Wezterm failed to " .. e, vim.log.levels.ERROR, {
-		title = "Wezterm",
-	})
-end
-
----@private
-local function exit_handler(msg)
-	---@param obj vim.SystemCompleted
-	return function(obj)
-		if obj.code ~= 0 then
-			err(msg)
-		end
-	end
-end
 local config = {
 	lsps = { "ruby_lsp", "rubocop" },
 	formatters = {},
 	linters = {},
 	test = function(cursor_row)
-		local wezterm = require("wezterm")
 		local current_file = vim.api.nvim_buf_get_name(0)
-		local bundle_directory =
-			vim.fn.fnamemodify(string.gsub(vim.fn.findfile("Gemfile", ".;"), "/Gemfile$", ""), ":p")
+		local root_patterns = { ".bundle", "Gemfile" }
+		local bundle_directory = vim.fs.dirname(vim.fs.find(root_patterns, { upward = true, path = current_file })[1])
 		vim.notify("Running tests in context: " .. vim.inspect(bundle_directory))
 
 		local files_to_test = {}
@@ -41,17 +25,8 @@ local config = {
 				end)
 				:totable()
 		end
-		local pane_id = wezterm.get_pane_direction("Right")
-		if pane_id ~= nil then
-			wezterm.exec_sync({ "cli", "kill-pane", "--pane-id", string.format("%d", pane_id) })
-		end
 
-		wezterm.exec({
-			"cli",
-			"split-pane",
-			"--right",
-			"--percent",
-			"15",
+		return {
 			"--cwd",
 			bundle_directory,
 			"--",
@@ -59,7 +34,7 @@ local config = {
 			"exec",
 			"rspec",
 			vim.iter(files_to_test):join(" "),
-		}, exit_handler("split pane"))
+		}
 	end,
 }
 
--- home-manager/common/neovim/lua/ducharmemp/terminal.lua
+++ home-manager/common/neovim/lua/ducharmemp/terminal.lua
@@ -6,6 +6,32 @@ local languages = require("ducharmemp.languages")
 ---@diagnostic disable-next-line: missing-parameter
 wezterm.setup()
 
+local last_command = nil
+local wezterm_spawn = { "cli", "split-pane", "--right", "--percent", "15" }
+
+local function err(e)
+	vim.notify("Wezterm failed to " .. e, vim.log.levels.ERROR, {
+		title = "Wezterm",
+	})
+end
+
+local function exit_handler(msg)
+	---@param obj vim.SystemCompleted
+	return function(obj)
+		if obj.code ~= 0 then
+			err(msg)
+		end
+	end
+end
+
+local function concat(t1, t2)
+	local t3 = { unpack(t1) }
+	for I = 1, #t2 do
+		t3[#t1 + I] = t2[I]
+	end
+	return t3
+end
+
 vim.keymap.set("n", "<leader>wt", function()
 	local needs_new_split = wezterm.get_pane_direction("Down") == nil
 	if needs_new_split then
@@ -20,10 +46,28 @@ vim.keymap.set("n", "<leader>tt", function()
 	local cursor_row, _cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
 
 	---@diagnostic disable-next-line: redundant-parameter
-	languages[filetype].test(cursor_row)
+	last_command = languages[filetype].test(cursor_row)
+	local pane_id = wezterm.get_pane_direction("Right")
+	if pane_id ~= nil then
+		wezterm.exec_sync({ "cli", "kill-pane", "--pane-id", string.format("%d", pane_id) })
+	end
+	wezterm.exec(concat(wezterm_spawn, last_command), exit_handler("split pane"))
 end, { desc = "Run tests closest to cursor" })
 
 vim.keymap.set("n", "<leader>tf", function()
 	local filetype = vim.bo.filetype
-	languages[filetype].test()
+	last_command = languages[filetype].test()
+	local pane_id = wezterm.get_pane_direction("Right")
+	if pane_id ~= nil then
+		wezterm.exec_sync({ "cli", "kill-pane", "--pane-id", string.format("%d", pane_id) })
+	end
+	wezterm.exec(concat(wezterm_spawn, last_command), exit_handler("split pane"))
 end, { desc = "Run tests for whole file" })
+
+vim.keymap.set("n", "<leader>tr", function()
+	local pane_id = wezterm.get_pane_direction("Right")
+	if pane_id ~= nil then
+		wezterm.exec_sync({ "cli", "kill-pane", "--pane-id", string.format("%d", pane_id) })
+	end
+	wezterm.exec(concat(wezterm_spawn, last_command), exit_handler("split pane"))
+end, { desc = "Repeat last test command" })
