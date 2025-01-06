2ba196c95a9f28e7c915cc1f795677035be43aa8
Matthew DuCharme <ducharmemp@gmail.com>
2025-01-06 09:46:31 -0500

    Work laptop

--- home-manager/common/fish.nix
+++ home-manager/common/fish.nix
@@ -4,6 +4,8 @@
         shellInit = ''
           set fish_greeting
           test -O ~/.aliases && source ~/.aliases
+          for p in $HOME/.local/share/gem/ruby/*; fish_add_path --prepend $p/bin; end
+          for p in /Applications/WezTerm.app/Contents/MacOS*; fish_add_path --prepend $p; end
           wezterm shell-completion --shell fish > $HOME/.config/fish/completions/wezterm.fish
           set -g fish_key_bindings fish_vi_key_bindings
         '';
