65b2270ea3717f6ddb1f35a6c5fd92fff94b08a7
Matthew DuCharme <ducharmemp@gmail.com>
2025-01-14 09:13:42 -0500

    Adding fancy spotify poller

--- home-manager/common/wezterm/wezterm.lua
+++ home-manager/common/wezterm/wezterm.lua
@@ -28,6 +28,82 @@ config.keys = {
 	},
 }
 
+config.status_update_interval = 5000
+wezterm.on("update-status", function(window, _)
+	local SOLID_LEFT_ARROW = ""
+	local success, stdout, stderr =
+		wezterm.run_child_process({ os.getenv("SHELL"), "-c", "spotify_player get key playback" })
+	print("stdout", stdout)
+	print("stderr", stderr)
+	print("success", success)
+	local parsed = wezterm.json_parse(stdout)
+	local playing_item = "No music playing"
+	local artists = ""
+	if parsed ~= nil then
+		playing_item = parsed["item"]["name"]
+		for i, artist in ipairs(parsed["item"]["artists"]) do
+			artists = artists .. artist["name"]
+			if i < #parsed["item"]["artists"] then
+				artists = artists .. ", "
+			end
+		end
+	end
+
+	local formatted_segment = playing_item
+
+	if artists ~= "" then
+		formatted_segment = playing_item .. " | " .. artists
+	end
+	local segments = {
+		wezterm.strftime("%a %b %d %H:%M"),
+		formatted_segment,
+		wezterm.hostname(),
+	}
+	local color_scheme = window:effective_config().resolved_palette
+	-- Note the use of wezterm.color.parse here, this returns
+	-- a Color object, which comes with functionality for lightening
+	-- or darkening the colour (amongst other things).
+	local bg = wezterm.color.parse(color_scheme.background)
+	local fg = color_scheme.foreground
+
+	-- Each powerline segment is going to be coloured progressively
+	-- darker/lighter depending on whether we're on a dark/light colour
+	-- scheme. Let's establish the "from" and "to" bounds of our gradient.
+	local gradient_to, gradient_from = bg
+	gradient_from = gradient_to:lighten(0.2)
+
+	-- Yes, WezTerm supports creating gradients, because why not?! Although
+	-- they'd usually be used for setting high fidelity gradients on your terminal's
+	-- background, we'll use them here to give us a sample of the powerline segment
+	-- colours we need.
+	local gradient = wezterm.color.gradient(
+		{
+			orientation = "Horizontal",
+			colors = { gradient_from, gradient_to },
+		},
+		#segments -- only gives us as many colours as we have segments.
+	)
+
+	-- We'll build up the elements to send to wezterm.format in this table.
+	local elements = {}
+
+	for i, seg in ipairs(segments) do
+		local is_first = i == 1
+
+		if is_first then
+			table.insert(elements, { Background = { Color = "none" } })
+		end
+		table.insert(elements, { Foreground = { Color = gradient[i] } })
+		table.insert(elements, { Text = SOLID_LEFT_ARROW })
+
+		table.insert(elements, { Foreground = { Color = fg } })
+		table.insert(elements, { Background = { Color = gradient[i] } })
+		table.insert(elements, { Text = " " .. seg .. " " })
+	end
+
+	window:set_right_status(wezterm.format(elements))
+end)
+
 -- This function returns the suggested title for a tab.
 -- It prefers the title that was set via `tab:set_title()`
 -- or `wezterm cli set-tab-title`, but falls back to the
