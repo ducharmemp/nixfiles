d6784cb2a4505f24a057eca5eee7e7915e5a262a
Matthew DuCharme <ducharmemp@gmail.com>
2025-02-20 11:50:19 -0500

    Tweaks

--- home-manager/common/fish.nix
+++ home-manager/common/fish.nix
@@ -5,11 +5,8 @@
         enable = true;
         shellInit = ''
           set fish_greeting
-          test -O ~/.aliases && source ~/.aliases
+          test -O ~/code/devbox/aliases.fish && source ~/code/devbox/aliases.fish
           for p in $HOME/.local/share/gem/ruby/*; fish_add_path --prepend $p/bin; end
-          for p in /Applications/WezTerm.app/Contents/MacOS*; fish_add_path --prepend $p; end
-          wezterm shell-completion --shell fish > $HOME/.config/fish/completions/wezterm.fish
-          set -g fish_key_bindings fish_vi_key_bindings
         '';
         plugins = [
             { name = "fzf-fish"; inherit (pkgs.fishPlugins.fzf-fish) src; }
