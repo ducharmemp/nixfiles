e14642e3e5f6573b190ff7037200cc4eb4aca925
Matthew DuCharme <ducharmemp@gmail.com>
2025-04-23 20:12:28 -0400

    Adding buildah, fish fixes

--- home-manager/common/fish.nix
+++ home-manager/common/fish.nix
@@ -7,6 +7,7 @@
           set fish_greeting
           test -O ~/code/devbox/aliases.fish && source ~/code/devbox/aliases.fish
           for p in $HOME/.local/share/gem/ruby/*; fish_add_path --prepend $p/bin; end
+          fish_add_path --prepend /run/wrappers/bin
         '';
         plugins = [
             { name = "fzf-fish"; inherit (pkgs.fishPlugins.fzf-fish) src; }
