f455a449512139b7ced9e7759682311e3c3505c6
Matthew DuCharme <ducharmemp@gmail.com>
2025-05-12 09:58:39 -0400

    Flake updates for work laptop

--- home-manager/common/fish.nix
+++ home-manager/common/fish.nix
@@ -8,9 +8,18 @@
           test -O ~/code/devbox/aliases.fish && source ~/code/devbox/aliases.fish
           for p in $HOME/.local/share/gem/ruby/*; fish_add_path --prepend $p/bin; end
           fish_add_path --prepend /run/wrappers/bin
+          set fzf_preview_dir_cmd eza --all --color=always
+          set fzf_diff_highlighter delta --paging=never --width=20
         '';
         plugins = [
             { name = "fzf-fish"; inherit (pkgs.fishPlugins.fzf-fish) src; }
         ];
-  };
+    };
+
+    home.packages = with pkgs; [
+        fzf
+        bat
+        eza
+        fd
+    ];
 }
