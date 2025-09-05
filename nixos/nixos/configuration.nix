f3b8ebc1a525a2e10ae504674e1daba67b77a864
Matthew DuCharme <ducharmemp@gmail.com>
2025-09-04 20:34:04 -0400

    Enabling zram

--- nixos/nixos/configuration.nix
+++ nixos/nixos/configuration.nix
@@ -35,6 +35,7 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  zramSwap.enable = true;
 
   # Enable networking
   networking.networkmanager.enable = true;
