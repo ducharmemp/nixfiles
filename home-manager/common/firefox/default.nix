90b1a38b42180d85a2e16437f1b60f9335d9c8a9
Matthew DuCharme <ducharmemp@gmail.com>
2025-09-14 12:26:36 -0400

    More tweaks

--- home-manager/common/firefox/default.nix
+++ home-manager/common/firefox/default.nix
@@ -17,12 +17,6 @@
         OfferToSaveLoginsDefault = false;
         PasswordManagerEnabled = false;
       };
-      extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [
-        ublock-origin
-        onepassword-password-manager
-        kagi-search
-        kagi-privacy-pass
-      ];
     };
   };
 }  
