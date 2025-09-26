5771dcb76205bf2b81381fdf237f46b92a784147
Matthew DuCharme <ducharmemp@gmail.com>
2025-09-26 09:33:57 -0400

    Fixes for blink config

--- home-manager/common/nvim/plugins/blink.nix
+++ home-manager/common/nvim/plugins/blink.nix
@@ -30,11 +30,11 @@ _: {
             score_offset = 100;
             opts = {
               prefix_min_len = 3;
-              context_size = 5;
-              max_filesize = "1M";
+              backend.context_size = 5;
+              backend.ripgrep.max_filesize = "1M";
               project_root_marker = ".git";
-              project_root_fallback = true;
-              search_casing = "--ignore-case";
+              backend.ripgrep.project_root_fallback = true;
+              backend.ripgrep.search_casing = "--ignore-case";
               additional_rg_options = {};
               fallback_to_regex_highlighting = true;
               ignore_paths = {};
