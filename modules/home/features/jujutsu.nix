566c04539f3edd22515507ab553a5d5f0bcf2da5
Matthew DuCharme <ducharmemp@gmail.com>
2026-04-09 22:21:44 -0400

    Added a commit message template to the jujutsu feature.

--- modules/home/features/jujutsu.nix
+++ modules/home/features/jujutsu.nix
@@ -16,6 +16,18 @@
           user.name = inputs.profile.user.name;
           user.email = inputs.profile.user.email;
 
+          templates.draft_commit_description = ''
+            concat(
+              coalesce(description, default_commit_description, "\n"),
+              surround(
+                "\nJJ: This commit contains the following changes:\n", "",
+                indent("JJ:     ", diff.stat(72)),
+              ),
+              "\nJJ: ignore-rest\n",
+              diff.git(),
+            )
+          '';
+
           # signing.behavior = "own";
           # signing.backend = "ssh";
         };
