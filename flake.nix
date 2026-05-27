a1242afe5796a52f8736797c41c1321e632913f9
Matthew DuCharme <ducharmemp@gmail.com>
2026-05-26 22:19:43 -0400

    Lock update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:nixos/nixpkgs/69cbb6b7b66d93fd1b239ecb95bdd3ac827daf24";
 
@@ -15,14 +15,14 @@
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     unstable-home-manager.url = "github:nix-community/home-manager/master";
     unstable-home-manager.inputs.nixpkgs.follows = "unstable";
 
     # Darwin
-    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.11";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-26.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
@@ -35,7 +35,7 @@
 
     expert.url = "github:elixir-lang/expert";
 
-    catppuccin.url = "github:catppuccin/nix/release-25.11";
+    catppuccin.url = "github:catppuccin/nix/release-26.05";
 
     profile.url = "git+ssh://git@github.com/ducharmemp/profile";
 
